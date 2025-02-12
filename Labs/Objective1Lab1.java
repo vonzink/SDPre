85826d35536ccc600c2afd997deb9e1cbd3325c1
Vonzink <vonzink@gmail.com>
2025-02-12 16:13:50 -0700

    complete Objective1Lab1

---
+++ Labs/Objective1Lab1.java
@@ -0,0 +1,44 @@
+
+//java comments like C
+
+
+/*public class ClassExample {
+
+	public static void main(String[] args) {
+
+	}
+
+} 
+
+public class SysOutExample {
+
+	public static void main(String[] args) {
+		System.out.println("These Wrods will be printed.");
+	}
+}
+
+public class MenuExample {
+
+	public static void main (String[] args) {
+		System.out.println("Welcome to My Bank Application!");
+		System.out.println("---------");
+		System.out.println("1) Check Balance");
+		System.out.println("2) Withdraw");
+		System.out.println("3) Deposit");
+		System.out.println("----");
+	}
+}
+
+*/
+public class Objective1Lab1 {
+	public static void main(String[] args) {
+		System.out.println("Hello, Skill Distillery");
+
+	}
+}
+
+
+
+//a semicolon is like the period at the end of a sentence. 
+
+
