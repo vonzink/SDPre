6c319f231c0e2909d8e433c64bc025c3db3eca26
Vonzink <vonzink@gmail.com>
2025-02-12 17:38:51 -0700

    Completed Objective2Lab3.java

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,13 +1,13 @@
 
 public class Objective2Lab3 {
 	public static void main(String[] args) {
-		int yearBorn = 1975
-		String monthBorn = "November"
-		boolean weekend = false
+		int yearBorn = 1975;
+		String monthBorn = "November";
+		boolean weekend = false;
 
-		system.out.println(yearBorn)
-		system.out.println(monthBorn)
-		system.out.println(weekend)
+		System.out.println(yearBorn);
+		System.out.println(monthBorn);
+		System.out.println(weekend);
 	}
 }
 
