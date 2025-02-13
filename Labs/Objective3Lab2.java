bbc0ecfc205a5698c306516c52234966191f5c3a
Vonzink <vonzink@gmail.com>
2025-02-13 12:24:25 -0700

    Completed Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,21 @@
+
+
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+
+		Double side1, side2, hypotenuse,a, b;
+
+		side1 = 8.0;
+		side2 = 10.0;
+
+		a = side1 * side1;
+		b = side2 * side2;
+		hypotenuse = Math.sqrt(a + b);
+
+		System.out.println(hypotenuse);
+
+	}
+}
+
+//odd that you dont declare var inside brackets.
+//dont have to import library's?
