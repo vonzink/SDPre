706cde85c1403eb58ae148dbd39e32bfb3d8a6d2
Vonzink <vonzink@gmail.com>
2025-02-12 17:26:15 -0700

    completed Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Zachary Zink";
+		int age = 49;
+		String state = "Colorado";
+		System.out.println(" Name: " + name + " Age: " + age + " State: " + state);
+	}
+}	
+
