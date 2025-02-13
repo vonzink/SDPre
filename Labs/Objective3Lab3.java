4b9bb9c1ba3ad707ae6ffc6869aed07c1f6a2155
Vonzink <vonzink@gmail.com>
2025-02-13 13:12:13 -0700

    Completed Objective3Lab3.java

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,16 @@
+
+public class Objective3Lab3 {
+	public static void main(String[] args) {
+
+		int first, second, third, answer;
+		first = 1 + (int)(Math.random() * 6);
+		second = 1 + (int)(Math.random() * 6);
+		third = 1 + (int)(Math.random() * 6);
+		answer = first + second + third; 
+
+	System.out.println(first+ " + "  +second+ " + " +third+ " = " +answer);
+
+	}
+}
+
+//i feel like there is a better way to do this with a loop or something, just dont know how.
