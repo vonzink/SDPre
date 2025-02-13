119d9edca535c4c1f0db951def22d7d044391078
Vonzink <vonzink@gmail.com>
2025-02-12 17:06:56 -0700

    completed Objective1Lab1.java

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,35 +1,4 @@
 
-//java comments like C
-
-
-/*public class ClassExample {
-
-	public static void main(String[] args) {
-
-	}
-
-} 
-
-public class SysOutExample {
-
-	public static void main(String[] args) {
-		System.out.println("These Wrods will be printed.");
-	}
-}
-
-public class MenuExample {
-
-	public static void main (String[] args) {
-		System.out.println("Welcome to My Bank Application!");
-		System.out.println("---------");
-		System.out.println("1) Check Balance");
-		System.out.println("2) Withdraw");
-		System.out.println("3) Deposit");
-		System.out.println("----");
-	}
-}
-
-*/
 public class Objective1Lab1 {
 	public static void main(String[] args) {
 		System.out.println("Hello, Skill Distillery");
@@ -37,8 +6,5 @@ public class Objective1Lab1 {
 	}
 }
 
-
-
-//a semicolon is like the period at the end of a sentence. 
 
 
