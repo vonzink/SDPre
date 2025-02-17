a04fb0fe7a557fc4a68bd91f3a5c2242fac59661
Vonzink <vonzink@gmail.com>
2025-02-16 21:05:54 -0700

    Completed Objective5Lab1.java

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,36 @@
+
+// import java.util.Scanner;
+
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    
+ //     Scanner input = new Scanner(System.in);
+//      System.out.println("enter the first number ");
+//      num1 = input.nextInt(); 
+
+ //     System.out.println("enter the second number ");
+ //     num2 = input.nextInt(); 
+
+ //     Fill in the following logic conditions
+ //     the logic is already in this, im confused. 
+    
+
+    if(num1 > num2) {
+ 
+        System.out.println(num1 + " is greater than " + num2);
+
+    }
+    else if(num2 > num1) {
+
+        System.out.println(num2 + " is greater than " + num1);
+
+    }
+    else {
+        System.out.println(num1 + " is equal to " + num1);
+    }
+  }
+}
+
