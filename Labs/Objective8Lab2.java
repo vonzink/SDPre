e11ab5689c06036daf4156814404ed4b57e4dac3
Vonzink <vonzink@gmail.com>
2025-02-17 15:33:16 -0700

    Completed Objective8Lab2.java

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,14 @@
+ 
+ import java.util.Scanner; 
+
+ public class Objective8Lab2{ 
+ 	public static void main (String[] args){
+
+ 		int j = 0;
+
+ 		for(int i = 0; i<=20; i++){
+ 			j += i;
+ 		}
+ 		System.out.println(j);
+ 	}
+}
