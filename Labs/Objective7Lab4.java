a26e727dee84185d7647556bd29c736700f24a82
Vonzink <vonzink@gmail.com>
2025-02-17 00:21:25 -0700

    Completed Objective7Lab4.java

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+
+public class Objective7Lab4 {
+	public static void main (String[] args) {
+
+	int count = 0; 
+	int add = 0;
+
+		while (count <= 20) {
+			add += count;
+			count += 1;
+		}
+		System.out.println(add);
+	}
+}
